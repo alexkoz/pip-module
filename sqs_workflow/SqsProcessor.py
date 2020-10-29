6c96c1eff80252911687918b40377d830cdf935e
Alexander Kozlov <alex.d.kozlov@gmail.com>
2020-10-29 14:56:01 +0300

    Fixes. Added e2e test.

--- sqs_workflow/SqsProcessor.py
+++ sqs_workflow/SqsProcessor.py
@@ -1,11 +1,8 @@
 import boto3
 import os
-from sqs_workflow.tests.QueueMock import QueueMock
 
 boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'],
                             region_name=os.environ['REGION_NAME'])
-
-# queue = QueueMock()
 
 
 class SqsProcessor:
@@ -14,7 +11,6 @@ class SqsProcessor:
     queue = sqs_client.Queue(os.environ['QUEUE_LINK'])
     queue_str = os.environ['QUEUE_LINK']
 
-
     def __init__(self):
         pass
 
@@ -22,12 +18,17 @@ class SqsProcessor:
         req_send = self.queue.send_message(QueueUrl=self.queue_str, MessageBody=message_body)
         print(req_send)
         print(message_body)
+        return req_send
 
     def receive_messages(self, max_number_of_messages: int):
-        req_receive = self.queue.receive_messages(QueueUrl=self.queue_str, MaxNumberOfMessages=max_number_of_messages)
-        print(req_receive)
-        return self
+        response_messages = self.queue.receive_messages(QueueUrl=self.queue_str,
+                                                        MaxNumberOfMessages=max_number_of_messages)
+        return response_messages
 
-    def delete_message(self, receipt_handle: str):
-        self.queue.delete_message(QueueUrl=self.queue_str, ReceiptHandle=receipt_handle)
-        return self
+    def delete_message(self, message):
+        message.delete()
+
+    def purge_queue(self):
+        sqs_client = boto3.client('sqs')
+        req_purge = sqs_client.purge_queue(QueueUrl=self.queue_str)
+        return req_purge
--- sqs_workflow/e2e-tests/test_e2e_sqsProcessor.py
+++ sqs_workflow/e2e-tests/test_e2e_sqsProcessor.py
@@ -1,13 +1,44 @@
 from unittest import TestCase
 from sqs_workflow.SqsProcessor import SqsProcessor
+import time
 
 
 class TestSqsProcessor(TestCase):
+
+    def pull_messages(self, processor: SqsProcessor, number_of_messages: int) -> list:
+        attemps = 0
+        list_of_messages = processor.receive_messages(number_of_messages)
+        while attemps < 7 and len(list_of_messages) < number_of_messages:
+            messages_received = processor.receive_messages(1)
+            if len(messages_received) > 0:
+                list_of_messages += messages_received
+                print('len list of messgrs = ', len(list_of_messages))
+            else:
+                attemps += 1
+                time.sleep(2)
+            print('attemps =', attemps)
+        if attemps == 3:
+            print('out of attemps')
+        return list_of_messages
+
     def test_e2e(self):
         processor = SqsProcessor()
 
-        req_receive = processor.receive_messages(2)
-        req_send = processor.send_message('test_message_body')
-        req_receive = processor.receive_messages(2)
+        processor.purge_queue()
+        req_receive = processor.receive_messages(5)
+        self.assertTrue(len(req_receive) == 0)
 
-        self.assertTrue(1 == 1)
+        for i in range(10):
+            processor.send_message('test_message_body_' + str(i))
+
+        req_receive = self.pull_messages(processor, 3)
+        self.assertTrue(len(req_receive) == 3)
+
+        for message in req_receive:
+            processor.delete_message(message)
+        print('len req_receive after delete = ', len(req_receive))
+
+        req_receive = self.pull_messages(processor, 10)
+        print('len req_receive = ', len(req_receive))
+
+        self.assertTrue(len(req_receive) == 7)
