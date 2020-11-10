da7c42460928468feee520617fd6879f14aa86e3
Vadim Osmolovski <osvadimos@gmail.com>
2020-11-10 10:09:29 +0300

    Add todo

---
+++ sqs_workflow/aids/pre-process/pre_process_floor.py
@@ -0,0 +1,6 @@
+
+#todo list objects from s3
+'s3://immoviewer-ai-test/storage/segmentation/pretty-floors_data_from_01.06.2020/'
+#todo parse json
+#todo save with panoUrl only
+'s3://immoviewer-ai-test/storage/segmentation/only-panos_data_from_01.06.2020/'
---
+++ sqs_workflow/aids/test/send_preprocessing_similarity_message.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# todo send json object as list
