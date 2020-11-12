cd92e4ed4a0ad1cbda5db026c1370e40e6595727
Alexander Kozlov <alex.d.kozlov@gmail.com>
2020-11-12 18:11:38 +0300

    Expanded test_assemble_results_into_document

--- sqs_workflow/tests/S3HelperMock.py
+++ sqs_workflow/tests/S3HelperMock.py
@@ -1,5 +1,6 @@
 import logging
 from sqs_workflow.aws.s3.S3Helper import S3Helper
+from sqs_workflow.utils.StringConstants import StringConstants
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
@@ -22,4 +23,9 @@ class S3HelperMock(S3Helper):
 
     def read_s3_object(self, s3_key) -> str:
         roombox_result = '[{"x": 134.97460548852348, "y": -81.00949136890486, "type": "corner"}, {"x": 44.89013987568783, "y": 60.57382621349592, "type": "corner"}, {"x": 44.89014792056024, "y": -81.02934636352592, "type": "corner"}, {"x": 135.09895116736456, "y": 60.46545130151028, "type": "corner"}, {"x": -45.19433210114725, "y": -81.00949490730395, "type": "corner"}, {"x": -135.02197578544292, "y": 60.3577862900935, "type": "corner"}, {"x": -135.0219838079359, "y": -80.98968655382886, "type": "corner"}, {"x": -45.31867775760836, "y": 60.46545483988379, "type": "corner"}]'
-        return roombox_result
+        door_detection_result = '[{"color": 16777215, "computedValues": {"width": 71.97554911272209}, "doorDirectionRight": true, "doorType": 1, "elementId": "beta", "id": "door_108", "pathCorner": 1, "pathDirection": 1, "points": [{"x": 35.80435909946351,"y": -17.074991679581473},{"x": 44.21902962059778,"y": -20.455211700643225},{"x": 44.21902962059778,"y": 29.503975135145815},{"x": 35.80435923214196,"y": 24.985153472533955}],"positionOffset": 0,"room": "54kbn8dnx4.JPG","rotationOffset": 0,"text": "Door","type": "door", "zOrder": 3}, \
+                                 {"color": 16777215, "computedValues": {"width": 71.97554911272209}, "doorDirectionRight": true, "doorType": 1, "elementId": "beta", "id": "door_110", "pathCorner": 1, "pathDirection": 1, "points": [{"x": 35.80435909946351,"y": -17.074991679581473},{"x": 44.21902962059778,"y": -20.455211700643225},{"x": 44.21902962059778,"y": 29.503975135145815},{"x": 35.80435923214196,"y": 24.985153472533955}],"positionOffset": 0,"room": "54kbn8dnx4.JPG","rotationOffset": 0,"text": "Door","type": "door", "zOrder": 3}]'
+        if StringConstants.ROOM_BOX_KEY in s3_key:
+            return roombox_result
+        elif StringConstants.DOOR_DETECTION_KEY in s3_key:
+            return door_detection_result
