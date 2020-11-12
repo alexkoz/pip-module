76893b1d116d589011314e95f79b5189d9425507
Vadim Osmolovski <osvadimos@gmail.com>
2020-11-12 14:48:43 +0300

    Refactoring

--- sqs_workflow/utils/StringConstants.py
+++ sqs_workflow/utils/StringConstants.py
@@ -4,12 +4,11 @@ class StringConstants:
     PRY_KEY: str = 'pry'
     DOCUMENT_PATH_KEY: str = "documentPath"
     STEPS_DOCUMENT_PATH_KEY: str = "stepsDocumentPath"
-    PANO_URL_KEY: str = 'panoUrl'
+    PANO_URL_KEY: str = 'fileUrl'
     PANOS_KEY: str = 'panos'
     STEPS: str = 'steps'
     R_MATRIX_KEY: str = 'R_MATRIX'
     SIMILARITY_KEY: str = 'SIMILARITY'
     DOOR_DETECTION_KEY: str = 'DOOR_DETECTION'
     ROOM_BOX_KEY: str = 'ROOM_BOX'
-    STEPS_DOCUMENT_PATH_KEY = 'stepsDocumentPath'
     INFERENCE_ID_KEY = 'inferenceId'
