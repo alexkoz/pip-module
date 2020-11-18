c32ffc80b8c73b88d5529852ada786ec895bbc12
Vadim Osmolovski <osvadimos@gmail.com>
2020-11-18 20:38:09 +0300

    Improve logging

--- sqs_workflow/aws/sqs/SqsProcessor.py
+++ sqs_workflow/aws/sqs/SqsProcessor.py
@@ -155,15 +155,15 @@ class SqsProcessor:
         logging.info(f"Finished processing and save result on s3.")
         return processing_result
 
-    def run_process(self, executable: str, script: str, message_body: str) -> str:
+    def run_process(self, executable: str, script: str, executable_params: str) -> str:
+        logging.info(f'Start processing executable:{executable} script:{script} params:{executable_params}')
         subprocess_result = subprocess.run([executable,
                                             script,
-                                            message_body],
+                                            executable_params],
                                            universal_newlines=True,
                                            capture_output=True)
         if not subprocess_result.returncode == 0:
-            at_moment_time = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            message = f'{at_moment_time}\n SQS processing has failed for process:{executable} script:{script} message:{message_body}.'
+            message = f'Process has failed for process:{executable} script:{script} message:{executable_params}.'
             self.alert_service.send_slack_message(message, 0)
         logging.info(f'subprocess code: {subprocess_result.returncode} output: {subprocess_result.stdout}')
         return subprocess_result.stdout
