311a0b146afb7d8c083476a377a160d360323431
Vadim Osmolovski <osvadimos@gmail.com>
2020-11-24 13:57:36 +0300

    Fix hash part in input path for preprocessing

--- sqs_workflow/utils/similarity/SimilarityProcessor.py
+++ sqs_workflow/utils/similarity/SimilarityProcessor.py
@@ -111,7 +111,11 @@ class SimilarityProcessor:
         logging.info(f"Start pre-processing message:{message_object}, input:{input_path}")
         list_messages = []
 
+        hash_directory = message_object[StringConstants.EXECUTABLE_PARAMS_KEY] \
+            .replace('--input_path', '') \
+            .split()[0].strip()
         document_absolute_path = os.path.join(input_path,
+                                              hash_directory,
                                               os.path.basename(message_object[StringConstants.DOCUMENT_PATH_KEY]))
         with open(document_absolute_path) as f:
             document = json.load(f)
